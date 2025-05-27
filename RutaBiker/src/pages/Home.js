7112e8d3a2350e697fb4c794c88a34ca3034e884
ritex-hub <80970227+ritex-hub@users.noreply.github.com>
2025-05-26 18:45:58 -0600

    Add files via upload

---
+++ RutaBiker/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Bienvenido a RutaBiker</h1>
+      <p>App para motociclistas con rutas seguras, alertas, comunidad y más.</p>
+    </div>
+  );
+}
